6a9e83da32e3ef3e68329e708bd5e57bf668a0fe
shoaibakhterqadri <shoaibakhter181422@gmail.com>
2023-07-17 10:54:09 +0500

    string interpolation is completed

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -5,5 +5,12 @@ import { Component } from "@angular/core";
     templateUrl:"./server.component.html",
 })
 
-export class ServerComponent{}
+export class ServerComponent{
+    age:number=21;
+    name:string='Shoaib';
+
+     myBio() {
+      return  this.name;
+    }
+}
 
