4cddcf449defe41496c38f814e671040f1a2b06b
shoaibakhterqadri <github@4dmagic.pk>
2023-07-20 18:55:20 +0500

    Basic Pipes in Angular

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,13 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls:['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-data:string='';
-getValue(item:any){
-  this.data=item;
-  console.log(item)
-}
  
+  data:string="Shoaib Akhter";
+  
+  todayDay:string=Date();
+
 }
