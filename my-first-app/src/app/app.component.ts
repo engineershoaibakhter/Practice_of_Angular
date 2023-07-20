6f668e2ee587a4987a42b76a745fda15c7d9673d
shoaibakhterqadri <github@4dmagic.pk>
2023-07-20 13:02:26 +0500

    Send Data from Parent to Child Component

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -8,16 +8,10 @@ import { NgForm } from '@angular/forms';
   styleUrls:['./app.component.css']
 })
 export class AppComponent {
-  title:string="Todo List";
-  list:any[]=[]
-  addTask(item:any){
-    console.log(item)
-   this.list.push({id:this.list.length,name:item})
-   console.log(this.list)
-  }
-  deleteItem(id:Number){
-   this.list= this.list.filter(item=>item.id!==id)
-  }
+data:any=10;
+random(){
+  this.data=Math.floor(Math.random()*10);
+}
   
 
   
--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     ServerComponent,
     ServersComponent,
     NavbarComponent,
+    ChildComponent,
   ],
   imports: [
     BrowserModule,
