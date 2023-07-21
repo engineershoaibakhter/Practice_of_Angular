c7161a1d8fd51ae87c5660a756b61bbc13f767b9
shoaibakhterqadri <github@4dmagic.pk>
2023-07-21 12:45:25 +0500

    Reactive Form is completed

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl,FormGroup,FormControlName } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
- 
-  userLogin(loginForm: any) {
-   
-    console.log(loginForm)
-
-   
+  loginForm=new FormGroup({
+    user:new FormControl('shoaib'),
+    password:new FormControl('Akhter'),
+  })
+  loginUser(){
+    console.log(this.loginForm.value);
   }
+ 
+
 }
--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -24,6 +25,7 @@ import { PkrDollarPipe } from './pipes/pkr-dollar.pipe';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
