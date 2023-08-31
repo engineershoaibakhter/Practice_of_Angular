34a6885bba461de0a301913ab6fd161d526794b2
shoaibakhterqadri <shoaibakhter181422@gmail.com>
2023-08-31 12:38:02 +0500

    template-reference-component is completed

--- learning/src/app/app.module.ts
+++ learning/src/app/app.module.ts
@@ -4,24 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChildComponent } from './child/child.component';
-import { DialogOverviewExampleComponent } from './dialog-overview-example/dialog-overview-example.component';
-import { DialogOverviewExampleDialogComponent } from './dialog-overview-example-dialog/dialog-overview-example-dialog.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
-import {NgIf, formatDate} from '@angular/common';
+import {MatDialogModule} from '@angular/material/dialog';
+import {NgIf} from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { CustomerListComponent } from './customer-list/customer-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent,
-    DialogOverviewExampleComponent,
-    DialogOverviewExampleDialogComponent
+    CustomerListComponent,
+    
   ],
   imports: [
     BrowserModule,
---
+++ learning/src/app/customer-list/customer.ts
@@ -0,0 +1,4 @@
+export class Customer{
+    customerNo:number=0;
+    firstName:string='Qadri';
+}
