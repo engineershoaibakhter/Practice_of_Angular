fd1ffb95ffbca6debdd288dc99d223aa97a63283
shoaibakhterqadri <shoaibakhter181422@gmail.com>
2023-09-08 08:43:16 +0500

    Error & Completion of Observer is completed

--- practice/src/app/app.component.ts
+++ practice/src/app/app.component.ts
@@ -10,17 +10,25 @@ export class AppComponent implements OnInit {
   myObservable = new Observable((Observer) => {
     console.log('Observer');
     setTimeout(()=>Observer.next("1"),1000);
-    setTimeout(()=>Observer.next("2"),5000);
+    setTimeout(()=>Observer.next("2"),2000);
+    // setTimeout(()=>Observer.error(new Error('Something went wrong')),3000);
+    setTimeout(()=>Observer.next("4"),4000);
+    setTimeout(()=>Observer.next("5"),5000);
+    setTimeout(()=>Observer.complete(),6000);
+    setTimeout(()=>Observer.next("7"),7000);
 
-    Observer.next('1');
-    Observer.next('2');
-    Observer.next('3');
+   
   });
 
   ngOnInit() {
     //  this.myObservable.subscribe(next,error,complete)
     this.myObservable.subscribe((data) => {
       console.log(data);
-    });
+    },(error)=>{
+      alert(error);
+    },()=>{
+      alert('Completion')
+    }
+    );
   }
 }
