bd7ba0b74a257c7223e33d6c6b6ea67a9a150da6
shoaibakhterqadri <shoaibakhter181422@gmail.com>
2023-10-04 10:27:55 +0500

    Fragment in Angular

--- learning/src/app/app.component.ts
+++ learning/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'learning';
+
+  constructor(private activateRoute:ActivatedRoute){
+  }
+
+  ngOnInit() {
+      this.activateRoute.fragment.subscribe((value)=>{
+        console.log(value)
+        this.jumpTo(value)
+      })
+  }
+
+  jumpTo(section:any){
+    document.getElementById(section)?.scrollIntoView({behavior:'smooth'})
+  }
 }
