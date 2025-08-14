6d075b32c5cd3aa6306fbd96e76c9657af75739b
Shoaib Akhter <shoaibakhter181422@gmail.com>
2025-08-14 12:17:29 +0500

    feat: build modules and components

--- Angular10/src/app/app.component.ts
+++ Angular10/src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'Angular';
+  name="Shoaib";
+  getName(){
+    return this.name +" Akhter"
+  }
+  obj={
+    firstName:"Shoaib",
+    lastName:"Akhter",
+    age:17
+  }
+  bio=['shoaib','akhter',17]
+  num1=10
+  num2=20
 }
