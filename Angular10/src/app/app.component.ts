0fb4a5c4eced1cee0b93cf1ba02f2abc931eb484
Shoaib Akhter <shoaibakhter181422@gmail.com>
2025-08-15 16:41:55 +0500

    feat: learn form

--- Angular10/src/app/app.component.ts
+++ Angular10/src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular';
-  boolean=false;
-  show='yes'
+
+  getValuesFromForm(value){
+    console.log(value)
+  }
 }
--- Angular10/src/app/app.module.ts
+++ Angular10/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthModule
+    AuthModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
