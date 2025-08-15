a14640efbbacd14852f2827076169dc7f03fbaf7
Shoaib Akhter <shoaibakhter181422@gmail.com>
2025-08-15 14:18:25 +0500

    feat: learn if else | conditional statement

--- Angular10/src/app/app.component.ts
+++ Angular10/src/app/app.component.ts
@@ -7,16 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular';
-  name="Shoaib";
-  getName(){
-    return this.name +" Akhter"
-  }
-  obj={
-    firstName:"Shoaib",
-    lastName:"Akhter",
-    age:17
-  }
-  bio=['shoaib','akhter',17]
-  num1=10
-  num2=20
+  boolean=false;
+  show='yes'
 }
